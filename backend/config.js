08f8f3043870e8f35fd0b01d0e1797f187899830
Fahad Nawaz <fahadnawaz@Fahads-MacBook-Air.local>
2024-06-29 01:34:24 +0500

    first commit

---
+++ backend/config.js
@@ -0,0 +1,11 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@books-store-mern.qstqcjc.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN";
+
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
+
+//mongodb+srv://root:<password>@books-store-mern.qstqcjc.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN
+
